beebedd00d58e5fd6813980e992e517191e230f0
mrkhurraam <mrkhurraam@gmail.com>
2026-02-14 03:56:45 +1100

    create product provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cart_with_riverpod/screens/home/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,14 @@
+import 'package:cart_with_riverpod/providers/product_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:cart_with_riverpod/shared/cart_icon.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allProducts = ref.watch(allProductProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sale Products'),
@@ -14,7 +17,7 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: GridView.builder(
-          itemCount: 8,
+          itemCount: allProducts.length,
           gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             mainAxisSpacing: 20,
@@ -22,9 +25,17 @@ class HomeScreen extends StatelessWidget {
             childAspectRatio: 0.9,
           ),
           itemBuilder:(context, index) {
+            final product = allProducts[index];
             return Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.blueGrey.withOpacity(0.05),
+                padding: const EdgeInsets.all(20),
+                color: Colors.blueGrey.withOpacity(0.05),
+                child: Column(
+                    children: [
+                      Image.asset(product.image, width: 60, height: 60),
+                      Text(product.title),
+                      Text('\$${product.price}'),
+                    ]
+                )
             );
           },
         ),
@@ -32,3 +43,5 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
